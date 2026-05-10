8ed2143962357b537842166c21af0353d3b04a62
ChauKimLuong <jinkun.116.05@gmail.com>
2026-05-10 18:12:39 +0700

    feat(auth): login with JWT

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { register, verifyOTP } = require("../controllers/authController");
+const { register, verifyOTP, login } = require("../controllers/auth.controller");
 const {
   validateRegister,
   validateVerifyOTP,
 } = require("../middleware/validation");
-const { registerLimiter } = require("../middleware/rateLimiter");
+const { registerLimiter, loginLimiter } = require("../middleware/rateLimiter");
 
 // POST /api/auth/register
 router.post("/register", registerLimiter, validateRegister, register);
@@ -13,4 +13,6 @@ router.post("/register", registerLimiter, validateRegister, register);
 // POST /api/auth/verify-otp
 router.post("/verify-otp", validateVerifyOTP, verifyOTP);
 
+// router.post("/login", loginLimiter, login);
+router.post("/login", login);
 module.exports = router;
